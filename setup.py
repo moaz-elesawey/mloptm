f2bd0510c6667acd4951b3d6175c9b309f971069
moaz-elesawey <mohammedmiaz3141@gmail.com>
2022-09-14 22:28:44 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -17,14 +17,26 @@ setup(
         author_email='mohammedmiaz3141@gmail.com',
         classifiers=[
             'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: MIT License',
+
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3 :: Only',
+            'License :: OSI Approved :: MIT License',
         ],
         keywords='python, python3, ml, optm, mloptm',
         package_dir={'': 'src'},
         packages=find_packages(where='src'),
         python_requires='>=3.7, <4',
+        install_requires=["numpy", "matplotlib", "tabulate", "Pillow"],
         project_urls={
         'Source': 'https://github.com/moaz-elesawey/mloptm',
     },
 )
+
+
