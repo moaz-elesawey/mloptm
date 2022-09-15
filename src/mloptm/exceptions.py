df3ef3d91afc88887df780f66f8ffc4f4a8c5d47
moaz-elesawey <mohammedmiaz3141@gmail.com>
2022-09-15 10:16:37 +0200

    fixied NotOptimizedError exception

--- src/mloptm/exceptions.py
+++ src/mloptm/exceptions.py
@@ -1,5 +1,5 @@
 class NotOptimizedError(Exception):
-    def __init__(self, message, errors):
+    def __init__(self, message, errors=None):
         super().__init__(message)
 
         self.errors = errors
