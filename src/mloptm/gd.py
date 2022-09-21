9ee41e5ca316b82183108d901eb136bab4526945
moaz-elesawey <mohammedmiaz3141@gmail.com>
2022-09-21 13:42:06 +0200

    added docstring for SteepestDescent class

--- src/mloptm/gd.py
+++ src/mloptm/gd.py
@@ -1,9 +1,6 @@
-### Multi Variable Gradient Descent Methods
-
 import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
-
 
 from mloptm.utils import EvalExpr
 from mloptm.methods import Golden
@@ -11,9 +8,70 @@ from mloptm.exceptions import NotOptimizedError
 
 
 class SteepestDescent:
-    def __init__(self, f, variables):
+    """
+    Steepeset Descent Minimizing Algorithm Implemenetation in Python
+
+    Attrs
+    -----
+     - f (str) : string representation of the function to minimize
+     - variables (list): list of the variables used in the function.
+     - maxiters (int): maximum number of iterations to run if we fail to converge.
+     - minima (list) : list of the minimum value for each variable.
+     - steps (list) : list of each step make in the minimizing process
+     - errors (list) : list of the errors at each step made.
+
+    Methods
+    -------
+     - Minimize
+     - PlotError
+     - PlotContour
+     - Plot3D
+
+    Examples
+    --------
+    >>> from mloptm.gd import SteepestDescent
+
+    >>> f = "cos(x^2 - 3*y) + sin(x^2 + y^2)"
+    >>> steepest = SteepestDescent(f=f, variables=("x", "y"))
+    >>> minima = steepest.Minimize(x0=(1, 1), eps=1e-7, verbose=True)
+    Running with Tolerance of [0.0000001000] for Max Iterations of [50]
+    Error at Iter [  1] = 0.424159602445
+    Error at Iter [  2] = 0.300241800580
+    Error at Iter [  3] = 0.020752222087
+    Error at Iter [  4] = 0.003268755730
+    Error at Iter [  5] = 0.000366715852
+    Error at Iter [  6] = 0.000049562070
+    Error at Iter [  7] = 0.000005521282
+    Error at Iter [  8] = 0.000000746351
+
+    >>> minima
+    ... array([[1.37638495],
+    ...        [1.67867607]])
+
+    >>> fig = steepest.PlotError(save=True, filename="errors.jpg")
+    >>> steepest.PlotContour(xdomain=(-3, 3), ydomain=(-3, 3), save=True, filename="contour.jpg")
+    >>> steepest.Plot3D(xdomain=(-3, 3), ydomain=(-3, 3), save=True, filename="3d.jpg")
+
+
+    >>> from mloptm.gd import SteepestDescent
+
+    >>> f = "x1^2 + x2^2 + x3^2 + x4^2"
+    >>> steepest = SteepestDescent(f=f, variables=("x1", "x2", "x3", "x4"))
+    >>> minima = steepest.Minimize(x0=(10, 10, 10, 10), eps=1e-7, verbose=False)
+
+    >>> minima
+    ... array([[-1.18032373e-16],
+    ...        [-1.18032373e-16],
+    ...        [-1.18032373e-16],
+    ...        [-1.18032373e-16]]
+
+    >>> fig = steepest.PlotError(save=True, filename="errors.jpg")
+    """
+
+    def __init__(self, f, variables, maxiters=50):
         self.f = f.replace("^", "**")
         self.variables = variables
+        self.maxiters = maxiters
 
         self.expr, self.symbols = EvalExpr(self.f, self.variables)
         self.expr_lambda = sp.lambdify([self.symbols], self.expr)
@@ -38,11 +96,16 @@ class SteepestDescent:
         ----------
          - x0 (list) : the initial point of the minimizing process
          - eps (float) : the tolerance to compare to the error from the minimizing process.
+         - verbose (bool) : whether to print information about each step or not.
 
         Rerturns
         --------
          - minima (list) : the minimum value of the function after the minimizing process.
         """
+
+        self.errors = []
+        self.steps = []
+        self._minima = None
 
         self.x0 = x0
 
@@ -55,19 +118,32 @@ class SteepestDescent:
         ## convert initial point to Matrix
         X0 = sp.Matrix(x0)
         self.steps.append(np.array(X0.T.tolist()[0], dtype=np.float64))
-        
+
+        ## evaulate the alpha expression
         alpha_evaluated = (X0 - alpha * grad).subs([ (var, val) for var, val in zip(self.symbols, x0) ]).n()
 
+        ## create a lambda function of the alpha expr to minimze it.
         alpha_lambda = sp.lambdify([alpha], self.expr.subs(
                                     [ (var, val) for var, val in 
                                      zip(self.symbols, alpha_evaluated) ]))
 
+        ## get the mimimum value of the alpha to get the larget step to take.
         minimum_alpha = Golden(alpha_lambda).Minimize(a0=-1, b0=1, eps=1e-6)
 
+        ## calculate the next point.
         xk = (X0 - minimum_alpha * grad).subs([ (var, val) for var, val in zip(self.symbols, X0) ]).n()
+
+        ## calculate the error between the last and next values.
+        err = (xk - X0).norm().n() / np.maximum(1, X0.norm().n())
+
+        ## append the results of both err and step to errors and steps.
+        self.errors.append(err)
         self.steps.append(np.array(xk.T.tolist()[0], dtype=np.float64))
 
-        for _ in range(50):
+        if verbose:
+            print("Running with Tolerance of [{:.10f}] for Max Iterations of [{}]".format(eps, self.maxiters))
+
+        for _ in range(self.maxiters):
             
             self.steps.append(np.array(xk.T.tolist()[0], dtype=np.float64))
             
@@ -87,19 +163,37 @@ class SteepestDescent:
             if err < eps: break
 
             if verbose:
-                print("Error at Iter [{:>6d}] = {:.12f}".format(_+1, err))
+                print("Error at Iter [{:>3d}] = {:.12f}".format(_+1, err))
             
             X0 = xk
 
-        self._minima = xk.n().tolist()
+        ## convert the lists to numpy arrays
+        self._minima = np.array(xk.n().tolist(), dtype=np.float64)
+        self.steps  = np.array(self.steps, dtype=np.float64)
+        self.errors = np.array(self.errors, dtype=np.float64)
 
         return self._minima
 
     def PlotError(self, **kw):
+        """
+        Generate a Line Plot of the Errors.
+
+        Parameters
+        ----------
+         - **kw : 
+
+        Returns
+        -------
+         - fig (plt.Figure) : the figure created.
+
+        Raises
+        ------
+         - NotOptimizedError
+        """
+
         if self.minima is None:
             raise NotOptimizedError("function did not minimized yet.")
 
-        self.errors = np.array(self.errors)
         iterations = range(1, self.errors.shape[0]+1)
         
         fig, ax = plt.subplots(nrows=1, figsize=(10, 6))
@@ -117,20 +211,36 @@ class SteepestDescent:
         return fig
 
     def PlotContour(self, xdomain, ydomain, **kw):
+        """
+        Generate a Contour Plot of the Function and Plot the Steps on it.
+
+        Parameters
+        ----------
+         - xdomain (list) : the lower and upper bound in the x axis.
+         - ydomain (list) : the lower and upper bound in the y axis.
+         - **kw : 
+
+        Returns
+        -------
+         - fig (plt.Figure) : the figure created.
+
+        Raises
+        ------
+         - NotOptimizedError
+         - ValueError
+        """
+
+
         if self.minima is None:
             raise NotOptimizedError("function did not minimized yet.")
 
         if len(self.symbols) != 2:
             raise ValueError("Can only be used to 3D systems, where symbols are just 2.")
 
-        self.steps = np.array(self.steps, dtype=np.float64)
         self.x_steps = self.steps[:, 0]
         self.y_steps = self.steps[:, 1]
 
         xmid, ymid = self.x0
-
-        # xs = np.linspace( xmid-xmid*2, xmid+xmid*2, 100 )
-        # ys = np.linspace( ymid-ymid*2, ymid+ymid*2, 100 )
 
         xs = np.linspace( *xdomain, 100 )
         ys = np.linspace( *ydomain, 100 )
@@ -159,13 +269,31 @@ class SteepestDescent:
         return fig
 
     def Plot3D(self, xdomain, ydomain, **kw):
+        """
+        Generate a 3D Plot of the Function and Plot the Steps on it.
+
+        Parameters
+        ----------
+         - xdomain (list) : the lower and upper bound in the x axis.
+         - ydomain (list) : the lower and upper bound in the y axis.
+         - **kw : 
+
+        Returns
+        -------
+         - fig (plt.Figure) : the figure created.
+
+        Raises
+        ------
+         - NotOptimizedError
+         - ValueError
+        """
+
         if self.minima is None:
             raise NotOptimizedError("function did not minimized yet.")
 
         if len(self.symbols) != 2:
             raise ValueError("Can only be used to 3D systems, where symbols are just 2.")
 
-        self.steps = np.array(self.steps, dtype=np.float64)
         self.x_steps = self.steps[:, 0]
         self.y_steps = self.steps[:, 1]
 
